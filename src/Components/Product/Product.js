3532b41f517a7a0e12bcf23b7e3f338eef204545
Your Name <you@example.com>
2022-09-25 17:32:05 +0200

    added

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,29 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+
+    // console.log(props.product);
+    const {name,img,seller,price,ratings} = props.product;
+// const {handleToAddCartClick} = props;
+    
+    return (
+        <div className='product'>
+           <img src={img} alt="" />
+           <div className='product-info'>
+           <p className='product-name'>{name}</p>
+           <p>price:{price}</p>
+           <p><small>Seller:{seller}</small></p>
+           <p><small>Ratings:{ratings} stars</small></p>
+           </div>
+           <button onClick={()=>props.handleToAddCartClick(props.product)} className='btn-cart'>
+            <p className='btn-text'>Add to Cart</p>
+            <FontAwesomeIcon icon={ faShoppingCart}></FontAwesomeIcon>
+           </button>
+        </div>
+    );
+};
+
+export default Product;
